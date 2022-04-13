e83103f9f87efc62a01839d600e4bf575736c1c4
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-13 20:55:02 +0530

    Added:Regex Problem to Validate Email Sample

---
+++ src/com/bridgelabz/regex_email/RegexValidation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.regex_email;
+
+public class RegexValidation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Java Regex problex to Validate Email Sample");
+
+	}
+
+}
